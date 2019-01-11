de5f59f06ab52fc3ccea4666ded54cf790920e46
Sajan Chandran <sajan.chandran@hmcts.net>
2019-01-11 16:44:10 +0000

    ATCM-3973 Event transformation for Defendant case updated event with empty updatedDate
    Change-Id: I198b3fd5ec1b2d8143700b4a39b50cad13e2032a

---
+++ sjp-domain/sjp-domain-transformation/sjp-domain-transformation-defendant-updated-date/run-transformation.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+TRANSFORMATION_JAR=`echo target/sjp-domain-transformation-defendant-updated-date-*.jar`
+EVENT_TOOL_VERSION=4.2.0
+EVENT_TOOL_JAR=target/event-tool-${EVENT_TOOL_VERSION}-swarm.jar
+
+PROCESS_FILE=target/processFile
+STANDALONE_XML=src/test/resources/standalone-ds.xml
+
+echo TRANSFORMATION_JAR=${TRANSFORMATION_JAR}
+
+[[ ! -f ${TRANSFORMATION_JAR} ]] && echo "File not found: "${TRANSFORMATION_JAR} && exit
+
+# Download Event Tool JAR
+[[ ! -f ${EVENT_TOOL_JAR} ]] && \
+    curl https://libraries-internal.mdv.cpp.nonlive/artifactory/repocentral/uk/gov/justice/event-tool/${EVENT_TOOL_VERSION}/event-tool-${EVENT_TOOL_VERSION}-swarm.jar -k > ${EVENT_TOOL_JAR}
+
+touch ${PROCESS_FILE}
+
+java -jar -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005 -Dorg.wildfly.swarm.mainProcessFile=${PROCESS_FILE} \
+    -Devent.transformation.jar=${TRANSFORMATION_JAR} ${EVENT_TOOL_JAR} \
+    -c ${STANDALONE_XML} -Dswarm.http.port=18080 -Dswarm.https.port=18443 -Dswarm.deployment.timeout=3600 \
+    | tee target/transformation.log
+

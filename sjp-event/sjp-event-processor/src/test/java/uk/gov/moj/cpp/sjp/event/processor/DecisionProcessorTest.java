24b78619990708ea4281cb664ea32b673123b862
Rakesh Chiluka <rakesh.chiluka@hmcts.net>
2019-03-25 16:18:53 +0000

    CRC-9875: schema and payload changes fixed review comments
    Change-Id: I97b8b0abb3c60f79f6a1b757e0b983494848abd5

--- sjp-event/sjp-event-processor/src/test/java/uk/gov/moj/cpp/sjp/event/processor/DecisionProcessorTest.java
+++ sjp-event/sjp-event-processor/src/test/java/uk/gov/moj/cpp/sjp/event/processor/DecisionProcessorTest.java
@@ -1,7 +1,11 @@
 package uk.gov.moj.cpp.sjp.event.processor;
 
+import static com.jayway.jsonpath.matchers.JsonPathMatchers.hasJsonPath;
 import static java.time.ZonedDateTime.now;
 import static java.util.UUID.randomUUID;
+import static javax.json.Json.createArrayBuilder;
+import static javax.json.Json.createObjectBuilder;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.Mockito.verify;
 import static uk.gov.justice.services.test.utils.core.enveloper.EnvelopeFactory.createEnvelope;
 
@@ -14,6 +18,8 @@ import uk.gov.moj.cpp.sjp.event.processor.activiti.CaseStateService;
 import java.util.UUID;
 
 import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -39,6 +45,7 @@ public class DecisionProcessorTest {
 
     private UUID caseId = randomUUID();
 
+
     @Test
     public void shouldUpdateCaseState() {
 
@@ -52,5 +59,44 @@ public class DecisionProcessorTest {
 
         verify(caseStateService).caseCompleted(caseId, event.metadata());
     }
+
+    @Test
+    public void shouldUpdateCaseStateWithEnrichedJsonPayload() {
+
+        final Integer accountDivisionCode = 199;
+        final Integer enforcingCourtCode = 100;
+
+        JsonObject payload = Json.createObjectBuilder()
+                .add("caseId", caseId.toString())
+                .add("resultedOn", now().toString())
+                .add("sjpSessionId", randomUUID().toString())
+                .add("verdict", "PSJ")
+                .add("accountDivisionCode",accountDivisionCode)
+                .add("enforcingCourtCode",enforcingCourtCode)
+                .add("offences", Json.createArrayBuilder().add(
+                        createObjectBuilder()
+                                .add("id", randomUUID().toString())
+                                .add("results",
+                                        createArrayBuilder().add(createObjectBuilder()
+                                                .add("code", "")
+                                                .add("resultTypeId", randomUUID().toString())
+                                                .add("terminalEntries", "")))
+                )).build();
+        final JsonEnvelope event = createEnvelope("public.resulting.referenced-decisions-saved",
+
+                        payload);
+        caseDecisionListener.referencedDecisionsSaved(event);
+
+        verify(caseStateService).caseCompleted(caseId, event.metadata());
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.accountDivisionCode"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.enforcingCourtCode"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.verdict"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.offences"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.offences[*].results"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.offences[*].results[*].code"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.offences[*].results[*].resultTypeId"));
+        assertThat(event.payloadAsJsonObject(), hasJsonPath("$.offences[*].results[*].terminalEntries"));
+    }
+
 
 }

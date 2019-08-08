cb548924c0fb5d75814aefc0b360ad692cc29b10
Arcadius Ahouansou <arcadius@menelic.com>
2019-08-08 07:20:54 +0100

    SCUS-238 : JOLT transformation of sjp.events.sjp-case-created
    Change-Id: Ie886406647aec58537b3c251b6e8b45a83931115

---
+++ sjp-event/sjp-event-indexer/src/test/java/uk/gov/moj/cpp/indexer/jolt/SjpCaseCreatedTransformationTest.java
@@ -0,0 +1,53 @@
+package uk.gov.moj.cpp.indexer.jolt;
+
+import static junit.framework.TestCase.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static uk.gov.moj.cpp.indexer.jolt.helper.JoltInstanceHelper.initializeJolt;
+import static uk.gov.moj.cpp.indexer.jolt.helper.JsonHelper.readJson;
+import static uk.gov.moj.cpp.indexer.jolt.helper.JsonHelper.readJsonViaPath;
+
+import uk.gov.justice.json.jolt.JoltTransformer;
+
+import java.io.IOException;
+
+import javax.json.JsonObject;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SjpCaseCreatedTransformationTest {
+
+
+    private final JoltTransformer joltTransformer = new JoltTransformer();
+
+    @Before
+    public void setUp() {
+        initializeJolt(joltTransformer);
+    }
+
+    @Test
+    public void shouldTransformSjpCaseCreatedEvent() throws IOException {
+
+        final JsonObject specJson = readJsonViaPath("src/transformer/sjp.events.sjp-case-created-spec.json");
+        assertNotNull(specJson);
+
+        final JsonObject inputJson = readJson("/sjp.events.sjp-case-created.json");
+        final JsonObject transformedJson = joltTransformer.transformWithJolt(specJson.toString(), inputJson);
+
+        verifyCase(transformedJson);
+    }
+
+    private void verifyCase(final JsonObject actualCase) {
+        assertThat(actualCase.getString("caseId"), is("7e2f843e-d639-40b3-8611-8015f3a18958"));
+        assertThat(actualCase.getString("caseReference"), is("22C22222222"));
+        assertThat(actualCase.getString("sjpNoticeServed"), is("2015-12-02"));
+        assertThat(actualCase.getString("prosecutingAuthority"), is("DVLA"));
+        assertThat(actualCase.getString("caseStatus"), is("NO_PLEA_RECEIVED"));
+        assertThat(actualCase.getString("_case_type"), is("prosecution"));
+        assertThat(actualCase.getBoolean("_is_sjp"), is(true));
+        assertThat(actualCase.getBoolean("_is_magistrates"), is(false));
+        assertThat(actualCase.getBoolean("_is_crown"), is(false));
+        assertThat(actualCase.getBoolean("_is_charging"), is(false));
+    }
+}
---
+++ sjp-integration-test/src/test/java/uk/gov/moj/sjp/it/test/ingestor/SjpCaseCreatedIngesterIT.java
@@ -0,0 +1,91 @@
+package uk.gov.moj.sjp.it.test.ingestor;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static uk.gov.justice.services.test.utils.core.messaging.JsonObjects.getJsonArray;
+import static uk.gov.moj.sjp.it.test.ingestor.helper.IngesterHelper.buildEnvelope;
+import static uk.gov.moj.sjp.it.test.ingestor.helper.IngesterHelper.jsonFromString;
+import static uk.gov.moj.sjp.it.util.FileUtil.getPayload;
+
+import uk.gov.justice.services.messaging.JsonEnvelope;
+import uk.gov.justice.services.test.utils.core.messaging.MessageProducerClient;
+import uk.gov.justice.services.test.utils.core.messaging.Poller;
+import uk.gov.moj.cpp.unifiedsearch.test.util.ingest.ElasticSearchClient;
+import uk.gov.moj.cpp.unifiedsearch.test.util.ingest.ElasticSearchIndexFinderUtil;
+import uk.gov.moj.cpp.unifiedsearch.test.util.ingest.ElasticSearchIndexRemoverUtil;
+import uk.gov.moj.sjp.it.test.BaseIntegrationTest;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import javax.json.JsonObject;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SjpCaseCreatedIngesterIT extends BaseIntegrationTest {
+    private static final String EVENT_NAME = "sjp.events.sjp-case-created";
+    private static final String PAYLOAD_PATH = "stub-data/sjp.events.sjp-case-created.json";
+    private static final String SJP_EVENT = "sjp.event";
+
+    private final MessageProducerClient privateEventsProducer = new MessageProducerClient();
+
+    private ElasticSearchIndexFinderUtil elasticSearchIndexFinderUtil;
+    private final Poller poller = new Poller(1200, 1000L);
+
+    @Before
+    public void setUp() throws IOException {
+        privateEventsProducer.startProducer(SJP_EVENT);
+        final ElasticSearchClient elasticSearchClient = new ElasticSearchClient();
+        elasticSearchIndexFinderUtil = new ElasticSearchIndexFinderUtil(elasticSearchClient);
+        new ElasticSearchIndexRemoverUtil().deleteAndCreateCaseIndex();
+    }
+
+    @After
+    public void tearDown() {
+        privateEventsProducer.close();
+    }
+
+    @Test
+    public void shouldIngestSjpCaseCreatedEvent() {
+        publishSjpCaseCreatedEvent();
+        final Optional<JsonObject> caseCreatedResponseObject = poller.pollUntilFound(() -> {
+            try {
+                final JsonObject jsonObject = elasticSearchIndexFinderUtil.findAll("crime_case_index");
+                if (jsonObject.getInt("totalResults") == 1) {
+                    return of(jsonObject);
+                }
+            } catch (final IOException e) {
+                fail();
+            }
+            return empty();
+        });
+
+        final JsonObject actualCase = jsonFromString(getJsonArray(caseCreatedResponseObject.get(), "index").get().getString(0));
+
+        verifyCase(actualCase);
+    }
+
+    private void verifyCase(final JsonObject actualCase) {
+        assertThat(actualCase.getString("caseId"), is("7e2f843e-d639-40b3-8611-8015f3a18958"));
+        assertThat(actualCase.getString("caseReference"), is("22C22222222"));
+        assertThat(actualCase.getString("sjpNoticeServed"), is("2015-12-02"));
+        assertThat(actualCase.getString("prosecutingAuthority"), is("DVLA"));
+        assertThat(actualCase.getString("caseStatus"), is("NO_PLEA_RECEIVED"));
+        assertThat(actualCase.getString("_case_type"), is("prosecution"));
+        assertThat(actualCase.getBoolean("_is_sjp"), is(true));
+        assertThat(actualCase.getBoolean("_is_magistrates"), is(false));
+        assertThat(actualCase.getBoolean("_is_crown"), is(false));
+        assertThat(actualCase.getBoolean("_is_charging"), is(false));
+    }
+
+    private void publishSjpCaseCreatedEvent() {
+        final String payload = getPayload(PAYLOAD_PATH);
+        final JsonEnvelope jsonEnvelope = buildEnvelope(payload, EVENT_NAME);
+        privateEventsProducer.sendMessage(EVENT_NAME, jsonEnvelope);
+    }
+}

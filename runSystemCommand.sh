d402c2067a3aa27c8821218e702de6bce29f70e4
amckenzie <allan.mckenzie@agilesphere.co.uk>
2019-11-21 14:36:37 +0000

    Update event-store to 2.4.1
    Change-Id: Ic31961ea1d973c68e8d33aebd8c0893ed44c1019

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.4.0
+FRAMEWORK_JMX_COMMAND_CLIENT_VERSION=2.4.1
 CONTEXT_NAME="sjp"
 USER_NAME="admin"
 PASSWORD="admin"

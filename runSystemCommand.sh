36f2f55615f4d4ebd35412586b59cf2c6ae2a501
zeldal <zeldal.ozdemir1@hmcts.net>
2025-05-23 13:58:11 +0100

    PEG-1777 Release Framework D Testing (#176)
    * PEG-1777 Release Framework D Testing
    
    * PEG-1777 fix fileservice, remove

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_VERSION=17.101.6
+FRAMEWORK_VERSION=17.103.0-M3
 CONTEXT_NAME="sjp"
 USER_NAME="admin"
 PASSWORD="admin"

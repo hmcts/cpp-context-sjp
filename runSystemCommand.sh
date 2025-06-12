95742263d424295c27cfa0055d8681cfaacbe9ce
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2025-06-12 08:19:39 +0100

    PEG-1778 Framework D M7

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_VERSION=17.103.0-M3
+FRAMEWORK_VERSION=17.103.0-M8
 CONTEXT_NAME="sjp"
 USER_NAME="admin"
 PASSWORD="admin"

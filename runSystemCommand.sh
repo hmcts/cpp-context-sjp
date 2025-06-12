165029d69df9136185c9b2664febde0a40735f97
zeldal <zeldal.ozdemir1@hmcts.net>
2025-06-12 17:39:22 +0100

    PEG-1846 Framework C upgrade (#211)

--- runSystemCommand.sh
+++ runSystemCommand.sh
@@ -17,7 +17,7 @@
 #
 ##################################################################################################
 
-FRAMEWORK_VERSION=17.103.0-M8
+FRAMEWORK_VERSION=17.103.0-M9
 CONTEXT_NAME="sjp"
 USER_NAME="admin"
 PASSWORD="admin"

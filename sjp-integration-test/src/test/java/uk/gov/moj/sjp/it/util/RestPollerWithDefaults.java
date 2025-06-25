867300340872190470cad739b64519e5db9c3246
zeldal-hmcts <zeldal.ozdemir1@hmcts.net>
2025-06-25 10:33:11 +0100

    PEG-1778 Framework D M11

--- sjp-integration-test/src/test/java/uk/gov/moj/sjp/it/util/RestPollerWithDefaults.java
+++ sjp-integration-test/src/test/java/uk/gov/moj/sjp/it/util/RestPollerWithDefaults.java
@@ -1,8 +1,6 @@
 package uk.gov.moj.sjp.it.util;
 
 
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
 import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
 import static javax.ws.rs.core.Response.Status.OK;
@@ -13,6 +11,7 @@ import static uk.gov.justice.services.test.utils.core.http.RestPoller.poll;
 import static uk.gov.justice.services.test.utils.core.matchers.ResponsePayloadMatcher.payload;
 import static uk.gov.justice.services.test.utils.core.matchers.ResponseStatusMatcher.status;
 
+import uk.gov.justice.services.test.utils.core.http.FibonacciPollWithStartAndMax;
 import uk.gov.justice.services.test.utils.core.http.RequestParams;
 import uk.gov.justice.services.test.utils.core.http.RequestParamsBuilder;
 import uk.gov.justice.services.test.utils.core.http.ResponseData;
@@ -22,6 +21,8 @@ import javax.json.JsonObject;
 
 import com.jayway.jsonpath.ReadContext;
 import org.hamcrest.Matcher;
+
+import java.time.Duration;
 
 public class RestPollerWithDefaults {
 
@@ -34,10 +35,9 @@ public class RestPollerWithDefaults {
     }
 
     public static RestPoller pollWithDefaults(final RequestParams requestParams) {
-        return poll(requestParams)
-                .timeout(TIMEOUT_IN_SECONDS, SECONDS)
-                .pollDelay(DELAY_IN_SECONDS, MILLISECONDS)
-                .pollInterval(INTERVAL_IN_SECONDS, SECONDS);
+        return poll(requestParams,
+                new FibonacciPollWithStartAndMax(Duration.ofMillis(10), Duration.ofMillis(100)),
+                Duration.ofSeconds(TIMEOUT_IN_SECONDS));
     }
 
     public static JsonObject pollWithDefaultsUntilResponseIsJson(final RequestParams requestParams, final Matcher<? super ReadContext> matcher) {

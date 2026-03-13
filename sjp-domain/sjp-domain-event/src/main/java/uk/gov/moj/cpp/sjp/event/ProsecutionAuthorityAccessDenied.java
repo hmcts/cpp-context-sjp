cf930b03c4608ebc5c3a08aaeb4f70dc9a19cc5b
Erol Kureli <>
2026-03-13 14:16:19 +0000

    CIMD-3294: refactor

--- sjp-domain/sjp-domain-event/src/main/java/uk/gov/moj/cpp/sjp/event/ProsecutionAuthorityAccessDenied.java
+++ sjp-domain/sjp-domain-event/src/main/java/uk/gov/moj/cpp/sjp/event/ProsecutionAuthorityAccessDenied.java
@@ -2,10 +2,13 @@ package uk.gov.moj.cpp.sjp.event;
 
 import uk.gov.justice.domain.annotation.Event;
 
+import java.io.Serializable;
 import java.util.List;
 
 @Event(ProsecutionAuthorityAccessDenied.EVENT_NAME)
-public class ProsecutionAuthorityAccessDenied {
+public class ProsecutionAuthorityAccessDenied  implements Serializable {
+    private static final long serialVersionUID = 1L;
+
     private final String caseAuthority;
     private final List<String> agentProsecutorAuthorityAccess;
 

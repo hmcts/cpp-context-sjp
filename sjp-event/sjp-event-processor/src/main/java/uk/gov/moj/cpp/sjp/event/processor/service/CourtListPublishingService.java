e5efc0812585c611454fffbc9d5ddb7995dd076b
yashpalnegi <yashpalkumar.negi@hmcts.net>
2026-08-13 17:29:37 +0100

    added more logging Transparency Report

--- sjp-event/sjp-event-processor/src/main/java/uk/gov/moj/cpp/sjp/event/processor/service/CourtListPublishingService.java
+++ sjp-event/sjp-event-processor/src/main/java/uk/gov/moj/cpp/sjp/event/processor/service/CourtListPublishingService.java
@@ -25,14 +25,13 @@ public class CourtListPublishingService {
         this.httpConnectionHelper = new HttpConnectionHelper();
     }
 
-    public Integer publishCourtList(final String payload) throws IOException {
+    public void publishCourtList(final String payload) throws IOException {
         final String url = applicationParameters.getCourtListPublishingServiceUrl() + PUBLISH_SJP_COURT_LIST_PATH;
         final String systemUserId = systemIdMapperService.getSystemUserId().toString();
         LOGGER.info("publishing court list to url {}, payload size {} bytes", url, payload.length());
         try {
             final Integer responseCode = httpConnectionHelper.getResponseCode(url, payload, systemUserId);
             LOGGER.info("publish court list response from url {}, responseCode {}", url, responseCode);
-            return responseCode;
         } catch (final IOException e) {
             LOGGER.error("failed to publish court list to url {}", url, e);
             throw e;

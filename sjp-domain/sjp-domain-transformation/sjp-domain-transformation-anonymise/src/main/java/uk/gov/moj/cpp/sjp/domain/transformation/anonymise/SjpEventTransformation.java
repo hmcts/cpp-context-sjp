81a7ca43dc1e59ddfdc0887265510a4a2f2108ef
allanmckenzie <allan.mckenzie@hmcts.net>
2026-08-28 14:48:20 +0100

    J25: remove deprecated sjp-domain-transformation-anonymise module
    The GDPR event-anonymisation tooling (stream-transformation-tool-anonymise)
    has no Jakarta EE 11 release - every published version is javax.json-based and
    calls the framework JsonEnvelope.envelopeFrom(Metadata, javax.json.JsonValue)
    overload that no longer exists on the Jakarta stack. The module is deprecated,
    so it is removed rather than carried as dead, un-runnable code.

--- sjp-domain/sjp-domain-transformation/sjp-domain-transformation-anonymise/src/main/java/uk/gov/moj/cpp/sjp/domain/transformation/anonymise/SjpEventTransformation.java
+++
@@ -1,8 +0,0 @@
-package uk.gov.moj.cpp.sjp.domain.transformation.anonymise;
-
-import uk.gov.justice.tools.eventsourcing.anonymization.EventAnonymiserTransformation;
-import uk.gov.justice.tools.eventsourcing.transformation.api.annotation.Transformation;
-
-@Transformation
-public class SjpEventTransformation extends EventAnonymiserTransformation {
-}
